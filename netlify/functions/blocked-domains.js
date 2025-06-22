8c0c1f9c7683ad9cdb5c9739f597c92d011d0255
Brylliant Dev <dev01@brylliantsolutions.com>
2025-06-22 20:39:53 +0800

    updated block list domain

--- netlify/functions/blocked-domains.js
+++ netlify/functions/blocked-domains.js
@@ -1,10 +1,37 @@
 // A centralized blocklist—add domains as you encounter them.
 module.exports = [
-    "gmail.com",
-    "yahoo.com",
-    "hotmail.com",
-    "outlook.com",
-    "vtext.com",
-    "registry.godaddy",
-    "registry.godaddy.com"
-  ];
+  // Major free/webmail providers
+  "gmail.com",
+  "yahoo.com",
+  "hotmail.com",
+  "outlook.com",
+  "live.com",
+  "aol.com",
+  "icloud.com",
+  "mail.com",
+  "gmx.com",
+  "protonmail.com",
+  "zoho.com",
+  "yandex.com",
+  "hushmail.com",
+  "tutanota.com",
+  "fastmail.com",
+
+  // Regional/public services
+  "mail.ru",
+  "qq.com",
+  "163.com",
+  "126.com",
+  "sina.com",
+
+  // Miscellaneous free domains
+  "inbox.com",
+  "lycos.com",
+  "rocketmail.com",
+  "att.net",
+  "bellsouth.net",
+  "vtext.com",
+  "registry.godaddy",
+  "registry.godaddy.com"
+];
+
